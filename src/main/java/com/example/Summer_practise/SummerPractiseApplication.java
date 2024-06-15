c5c377add5ec2f2859375185f64a9f1b7f9023ae
Alexandr20i <Alexandr20i@yandex.ru>
2024-06-15 11:31:03 +0400

    v_0

---
+++ src/main/java/com/example/Summer_practise/SummerPractiseApplication.java
@@ -0,0 +1,13 @@
+package com.example.Summer_practise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerPractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummerPractiseApplication.class, args);
+	}
+
+}
